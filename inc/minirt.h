997d8ff88f0525398d7118957f223870ab3dc6c4
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-17 21:53:15 -0300

    fix tuple constructors parameter type

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 10:24:24 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/17 16:51:40 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/17 21:32:36 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ typedef struct s_tuple
 typedef t_tuple	t_vector; /* tuple with 'w' value equal to 0 */
 typedef t_tuple	t_point; /* tuple with 'w' value equal to 1 */
 
-t_tuple		*create_tuple(int x, int y, int z, int w);
-t_point		*create_point(int x, int y, int z);
-t_vector	*create_vector(int x, int y, int z);
+t_tuple		*create_tuple(double x, double y, double z, double w);
+t_point		*create_point(double x, double y, double z);
+t_vector	*create_vector(double x, double y, double z);
 
 t_bool		is_equal(double a, double b);
 t_bool		is_equal_tuple(t_tuple *a, t_tuple *b);
--- src/tuple.c
+++ src/tuple.c
@@ -6,23 +6,23 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 10:23:27 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/17 14:31:30 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/17 21:32:36 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-t_point	*create_point(int x, int y, int z)
+t_point	*create_point(double x, double y, double z)
 {
 	return (create_tuple(x, y, z, 1));
 }
 
-t_vector	*create_vector(int x, int y, int z)
+t_vector	*create_vector(double x, double y, double z)
 {
 	return (create_tuple(x, y, z, 0));
 }
 
-t_tuple	*create_tuple(int x, int y, int z, int w)
+t_tuple	*create_tuple(double x, double y, double z, double w)
 {
 	t_tuple	*tuple;
 
