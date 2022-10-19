835a45ed8a9561080408daaed203ccf068fba628
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-19 18:49:18 -0300

    add canvas tests

--- tests/tests.c
+++ tests/tests.c
@@ -6,12 +6,11 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:05:18 by maolivei          #+#    #+#             */
-/*   Updated: 2022/10/19 15:34:09 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/19 18:15:57 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unity/unity.h"
-#include "tests.h"
+#include <tests.h>
 
 void	setUp(void)
 {
@@ -25,6 +24,7 @@ void	tearDown(void)
 
 int	main(void)
 {
+	srand(time(NULL));
 	UNITY_BEGIN();
 	test_tuple_operations();
 	test_colors();
@@ -33,5 +33,6 @@ int	main(void)
 	test_check_light();
 	test_check_ambient();
 	test_check_camera();
+	test_canvas();
 	return (UNITY_END());
 }
--- tests/tests.h
+++ tests/tests.h
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:25:39 by maolivei          #+#    #+#             */
-/*   Updated: 2022/10/19 14:18:15 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/19 17:44:15 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@
 #define LOOP_ITERATIONS 100
 
 void	test_tuple_operations(void);
+void	test_colors(void);
+void	test_canvas(void);
 void	pre_parser_errors(void);
 void	test_parser(void);
 void	test_check_light(void);
 void	test_check_ambient(void);
 void	test_check_camera(void);
-void	test_colors(void);
 
 #endif /* TESTS_H */
