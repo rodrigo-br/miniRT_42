b489cc94ec0ee0326b5061a522c3aa44aea10aab
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-19 22:24:17 -0300

    relabeling on canvas tests

--- tests/test_canvas.c
+++ tests/test_canvas.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 17:42:58 by maolivei          #+#    #+#             */
-/*   Updated: 2022/10/19 18:45:31 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/19 21:58:01 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	test_canvas_creation(void)
 	canvas = create_canvas(mlx, 500, 500);
 	TEST_ASSERT_NOT_NULL(canvas);
 	TEST_ASSERT_NOT_NULL(canvas->image);
-	TEST_ASSERT_NOT_NULL(canvas->addr);
+	TEST_ASSERT_NOT_NULL(canvas->address);
 	mlx_destroy_image(mlx, canvas->image);
 	mlx_destroy_display(mlx);
 	free(mlx);
@@ -43,8 +43,9 @@ void	test_canvas_painting(void)
 	canvas = create_canvas(mlx, 500, 500);
 	rgb = create_color(255, 0, 0);
 	write_to_canvas(canvas, 250, 250, rgb);
-	pixel = canvas->addr + (250 * canvas->line_len) + (250 * (canvas->bpp / 8));
-	TEST_ASSERT_EQUAL_HEX(0xFF0000FF, *(int *)pixel);
+	pixel = canvas->address;
+	pixel += (250 * canvas->line_length) + (250 * (canvas->bits_per_pixel / 8));
+	TEST_ASSERT_EQUAL_HEX(0xFF0000, *(int *)pixel);
 	mlx_destroy_image(mlx, canvas->image);
 	mlx_destroy_display(mlx);
 	free(mlx);
--- tests/tests.c
+++ tests/tests.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:05:18 by maolivei          #+#    #+#             */
-/*   Updated: 2022/10/19 18:15:57 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/19 22:19:27 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ int	main(void)
 	UNITY_BEGIN();
 	test_tuple_operations();
 	test_colors();
+	test_canvas();
 	pre_parser_errors();
 	test_atod();
 	test_check_light();
 	test_check_ambient();
 	test_check_camera();
-	test_canvas();
 	return (UNITY_END());
 }
