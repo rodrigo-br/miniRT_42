6d184fce847bc4b3eaae464146ee413055073195
rodrigo-br <rodrigoab123@gmail.com>
2022-10-21 11:03:19 -0300

    created translate_inverse_matrix and tested. Fixed invalid free when vector is passed instead of a point

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 10:24:24 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/21 10:06:16 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/10/21 10:57:36 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,5 +107,7 @@ t_matrix	*transpose_matrix(t_matrix *m);
 
 /* Transformation */
 t_tuple		*translate_matrix(t_tuple *point, t_tuple *translation);
+t_tuple		*translate_inverse_matrix(t_tuple *point, t_tuple *translation);
+t_tuple		*create_copy_tuple(t_tuple *t);
 
 #endif /* MINIRT_H */
--- src/transformations/translate_matrix.c
+++ src/transformations/translate_matrix.c
@@ -6,11 +6,16 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 08:49:18 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/21 09:55:02 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/10/21 11:02:31 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
+
+t_tuple	*create_copy_tuple(t_tuple *t)
+{
+	return (create_tuple(t->x, t->y, t->z, t->w));
+}
 
 t_tuple	*translate_matrix(t_tuple *point, t_tuple *translation)
 {
@@ -18,9 +23,25 @@ t_tuple	*translate_matrix(t_tuple *point, t_tuple *translation)
 	t_tuple	*new_tuple;
 
 	if (is_vector(point))
-		return (point);
+		return (create_copy_tuple(point));
 	temp = point->w;
 	new_tuple = sum_tuple(point, translation);
 	new_tuple->w = temp;
+	return (new_tuple);
+}
+
+t_tuple	*translate_inverse_matrix(t_tuple *point, t_tuple *translation)
+{
+	double	temp;
+	t_tuple	*new_tuple;
+	t_tuple	*inverse;
+
+	if (is_vector(point))
+		return (create_copy_tuple(point));
+	temp = point->w;
+	inverse = neg_tuple(translation);
+	new_tuple = sum_tuple(point, inverse);
+	new_tuple->w = temp;
+	free(inverse);
 	return (new_tuple);
 }
