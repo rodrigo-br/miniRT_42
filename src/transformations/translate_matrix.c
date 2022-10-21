be019063083ed82fb63d07e32c0dfc990ce9ecbf
rodrigo-br <rodrigoab123@gmail.com>
2022-10-21 12:14:50 -0300

    translate_universe_matrix fixed and testes fixed as well

--- src/transformations/translate_matrix.c
+++ src/transformations/translate_matrix.c
@@ -6,7 +6,7 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 08:49:18 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/21 12:00:03 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/10/21 12:08:07 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,26 @@ t_matrix	*translate_matrix(t_tuple *point, t_tuple *translation)
 	return (matrix);
 }
 
-// t_tuple	*translate_inverse_matrix(t_tuple *point, t_tuple *translation)
-// {
-// 	double	temp;
-// 	t_tuple	*new_tuple;
-// 	t_tuple	*inverse;
+t_matrix	*translate_inverse_matrix(t_tuple *point, t_tuple *translation)
+{
+	t_tuple		*new_tuple;
+	t_tuple		*inverse;
+	t_matrix	*matrix;
 
-// 	if (is_vector(point))
-// 		return (create_copy_tuple(point));
-// 	temp = point->w;
-// 	inverse = neg_tuple(translation);
-// 	new_tuple = sum_tuple(point, inverse);
-// 	new_tuple->w = temp;
-// 	free(inverse);
-// 	return (new_tuple);
-// }
+	matrix = create_identity_matrix();
+	if (is_vector(point))
+		new_tuple = point;
+	else
+	{
+		inverse = neg_tuple(translation);
+		new_tuple = sum_tuple(point, inverse);
+		free(inverse);
+	}
+	matrix->matrix[0][3] = new_tuple->x;
+	matrix->matrix[1][3] = new_tuple->y;
+	matrix->matrix[2][3] = new_tuple->z;
+	if (point->w == POINT_W)
+		free(new_tuple);
+	matrix->matrix[3][3] = point->w;
+	return (matrix);
+}
--- tests/test_transformations.c
+++ tests/test_transformations.c
@@ -21,7 +21,7 @@ void	transform_point_tuple(void)
 		TEST_ASSERT_EQUAL_DOUBLE((a->x + b->x), matrix->matrix[0][3]);
 		TEST_ASSERT_EQUAL_DOUBLE((a->y + b->y), matrix->matrix[1][3]);
 		TEST_ASSERT_EQUAL_DOUBLE((a->z + b->z), matrix->matrix[2][3]);
-		TEST_ASSERT_EQUAL_DOUBLE(1, rand_w);
+		TEST_ASSERT_EQUAL_DOUBLE(a->w, rand_w);
 		free(a);
 		free(b);
 		free(matrix);
@@ -42,109 +42,128 @@ void	transform_point_tuple(void)
 	free(matrix);
 }
 
-// void inverse_transform_point_tuple(void)
-// {
-// 	double	rand_x, rand_y, rand_z, rand_w;
-// 	t_tuple	*a, *b, *translate;
-// 	for (int i = 0; i < LOOP_ITERATIONS; i++)
-// 	{
-// 		rand_x = double_rand();
-// 		rand_y = double_rand();
-// 		rand_z = double_rand();
-// 		rand_w = 1;
-// 		a = create_tuple(rand_x, rand_y, rand_z, rand_w);
-// 		b = create_tuple(rand_x, rand_y, rand_z, rand_w);
-// 		translate = translate_inverse_matrix(a, b);
-// 		TEST_ASSERT_EQUAL_DOUBLE((a->x + -b->x), translate->x);
-// 		TEST_ASSERT_EQUAL_DOUBLE((a->y + -b->y), translate->y);
-// 		TEST_ASSERT_EQUAL_DOUBLE((a->z + -b->z), translate->z);
-// 		TEST_ASSERT_EQUAL_DOUBLE(rand_w, 1);
-// 		free(a);
-// 		free(b);
-// 		free(translate);
-// 	}
-// 	/*Scenario: Multiplying by the inverse of a translation matrix
-// 	Given transform ← translation(5, -3, 2)
-// 	And inv ← inverse(transform)
-// 	And p ← point(-3, 4, 5)
-// 	Then inv * p = point(-8, 7, 3)*/
-// 	a = create_point(-3, 4, 5);
-// 	b = create_tuple(5, -3, 2, 0);
-// 	translate = translate_inverse_matrix(a, b);
-// 	TEST_ASSERT_EQUAL_DOUBLE(-8, translate->x);
-// 	TEST_ASSERT_EQUAL_DOUBLE(7, translate->y);
-// 	TEST_ASSERT_EQUAL_DOUBLE(3, translate->z);
-// 	TEST_ASSERT_EQUAL_DOUBLE(1, translate->w);
-// 	free(a);
-// 	free(b);
-// 	free(translate);
-// }
+void inverse_transform_point_tuple(void)
+{
+	double	rand_x, rand_y, rand_z, rand_w;
+	t_tuple	*a, *b;
+	t_matrix	*matrix;
 
-// void	transform_vector_tuple(void)
-// {
-// 	double	rand_x, rand_y, rand_z, rand_w;
-// 	t_tuple	*a, *b, *translate;
+	for (int i = 0; i < LOOP_ITERATIONS; i++)
+	{
+		rand_x = double_rand();
+		rand_y = double_rand();
+		rand_z = double_rand();
+		rand_w = 1;
+		a = create_point(rand_x, rand_y, rand_z);
+		b = create_tuple(rand_x, rand_y, rand_z, rand_w);
+		matrix = translate_inverse_matrix(a, b);
+		TEST_ASSERT_EQUAL_DOUBLE((a->x + -b->x), matrix->matrix[0][3]);
+		TEST_ASSERT_EQUAL_DOUBLE((a->y + -b->y), matrix->matrix[1][3]);
+		TEST_ASSERT_EQUAL_DOUBLE((a->z + -b->z), matrix->matrix[2][3]);
+		TEST_ASSERT_EQUAL_DOUBLE((a->w), matrix->matrix[3][3]);
+		free(a);
+		free(b);
+		free(matrix);
+	}
+	/*Scenario: Multiplying by the inverse of a translation matrix
+	Given transform ← translation(5, -3, 2)
+	And inv ← inverse(transform)
+	And p ← point(-3, 4, 5)
+	Then inv * p = point(-8, 7, 3)*/
+	a = create_point(-3, 4, 5);
+	b = create_tuple(5, -3, 2, 0);
+	matrix = translate_inverse_matrix(a, b);
+	TEST_ASSERT_EQUAL_DOUBLE(-8, matrix->matrix[0][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(7, matrix->matrix[1][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(3, matrix->matrix[2][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(1, matrix->matrix[3][3]);
+	free(a);
+	free(b);
+	free(matrix);
+}
 
-// 	for (int i = 0; i < LOOP_ITERATIONS; i++)
-// 	{
-// 		rand_x = double_rand();
-// 		rand_y = double_rand();
-// 		rand_z = double_rand();
-// 		rand_w = 1;
-// 		a = create_vector(rand_x, rand_y, rand_z);
-// 		b = create_tuple(rand_x, rand_y, rand_z, rand_w);
-// 		translate = translate_matrix(a, b);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->x, translate->x);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->y, translate->y);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->z, translate->z);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->w, translate->w);
-// 		free(a);
-// 		free(b);
-// 		free(translate);
-// 	}
-// 	/*Scenario: Translation does not affect vectors
-// 	Given transform ← translation(5, -3, 2)
-// 	And v ← vector(-3, 4, 5)
-// 	Then transform * v = v*/
-// 	a = create_vector(-3, 4, 5);
-// 	b = create_point(5, -3, 2);
-// 	translate = translate_matrix(a, b);
-// 	TEST_ASSERT_EQUAL_DOUBLE(-3, translate->x);
-// 	TEST_ASSERT_EQUAL_DOUBLE(4, translate->y);
-// 	TEST_ASSERT_EQUAL_DOUBLE(5, translate->z);
-// 	TEST_ASSERT_EQUAL_DOUBLE(0, translate->w);
-// 	free(a);
-// 	free(b);
-// 	free(translate);
-// }
+void	transform_vector_tuple(void)
+{
+	double	rand_x, rand_y, rand_z, rand_w;
+	t_tuple	*a, *b;
+	t_matrix	*matrix;
 
-// void inverse_transform_vector_tuple(void)
-// {
-// 	double	rand_x, rand_y, rand_z, rand_w;
-// 	t_tuple	*a, *b, *translate;
-// 	for (int i = 0; i < LOOP_ITERATIONS; i++)
-// 	{
-// 		rand_x = double_rand();
-// 		rand_y = double_rand();
-// 		rand_z = double_rand();
-// 		rand_w = 1;
-// 		a = create_vector(rand_x, rand_y, rand_z);
-// 		b = create_tuple(rand_x, rand_y, rand_z, rand_w);
-// 		translate = translate_inverse_matrix(a, b);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->x, translate->x);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->y, translate->y);
-// 		TEST_ASSERT_EQUAL_DOUBLE(a->z, translate->z);
-// 		TEST_ASSERT_EQUAL_DOUBLE(rand_w, 1);
-// 		free(a);
-// 		free(b);
-// 		free(translate);
-// 	}
-// }
+	for (int i = 0; i < LOOP_ITERATIONS; i++)
+	{
+		rand_x = double_rand();
+		rand_y = double_rand();
+		rand_z = double_rand();
+		rand_w = 1;
+		a = create_vector(rand_x, rand_y, rand_z);
+		b = create_tuple(rand_x, rand_y, rand_z, rand_w);
+		matrix = translate_matrix(a, b);
+		TEST_ASSERT_EQUAL_DOUBLE(a->x, matrix->matrix[0][3]);
+		TEST_ASSERT_EQUAL_DOUBLE(a->y, matrix->matrix[1][3]);
+		TEST_ASSERT_EQUAL_DOUBLE(a->z, matrix->matrix[2][3]);
+		TEST_ASSERT_EQUAL_DOUBLE(a->w, matrix->matrix[3][3]);
+		free(a);
+		free(b);
+		free(matrix);
+	}
+	/*Scenario: Translation does not affect vectors
+	Given transform ← translation(5, -3, 2)
+	And v ← vector(-3, 4, 5)
+	Then transform * v = v*/
+	a = create_vector(-3, 4, 5);
+	b = create_tuple(5, -3, 2, 0);
+	matrix = translate_matrix(a, b);
+	TEST_ASSERT_EQUAL_DOUBLE(-3, matrix->matrix[0][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(4, matrix->matrix[1][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(5, matrix->matrix[2][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(0, matrix->matrix[3][3]);
+	free(a);
+	free(b);
+	free(matrix);
+}
+
+void inverse_transform_vector_tuple(void)
+{
+	double	rand_x, rand_y, rand_z, rand_w;
+	t_tuple	*a, *b;
+	t_matrix	*matrix;
+
+	for (int i = 0; i < LOOP_ITERATIONS; i++)
+	{
+		rand_x = double_rand();
+		rand_y = double_rand();
+		rand_z = double_rand();
+		rand_w = 1;
+		a = create_vector(rand_x, rand_y, rand_z);
+		b = create_tuple(rand_x, rand_y, rand_z, rand_w);
+		matrix = translate_inverse_matrix(a, b);
+		TEST_ASSERT_EQUAL_DOUBLE(a->x, matrix->matrix[0][3]);
+		TEST_ASSERT_EQUAL_DOUBLE(a->y, matrix->matrix[1][3]);
+		TEST_ASSERT_EQUAL_DOUBLE(a->z, matrix->matrix[2][3]);
+		TEST_ASSERT_EQUAL_DOUBLE(a->w, matrix->matrix[3][3]);
+		free(a);
+		free(b);
+		free(matrix);
+	}
+	/*Scenario: Translation does not affect vectors
+	Given transform ← translation(5, -3, 2)
+	And v ← vector(-3, 4, 5)
+	Then transform * v = v*/
+	a = create_vector(-3, 4, 5);
+	b = create_tuple(5, -3, 2, 0);
+	matrix = translate_inverse_matrix(a, b);
+	TEST_ASSERT_EQUAL_DOUBLE(-3, matrix->matrix[0][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(4, matrix->matrix[1][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(5, matrix->matrix[2][3]);
+	TEST_ASSERT_EQUAL_DOUBLE(0, matrix->matrix[3][3]);
+	free(a);
+	free(b);
+	free(matrix);
+}
 
 void test_transformations(void)
 {
 	RUN_TEST(transform_point_tuple);
-	// RUN_TEST(inverse_transform_point_tuple);
-	// RUN_TEST(transform_vector_tuple);
-	// RUN_TEST(inverse_transform_vector_tuple);
+	RUN_TEST(inverse_transform_point_tuple);
+	RUN_TEST(transform_vector_tuple);
+	RUN_TEST(inverse_transform_vector_tuple);
 }
