62ebf119da5471452a5575ab4fcf163724c747ef
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-26 09:45:50 -0300

    group same type functions in matrix checker

--- src/matrix/matrix_checker.c
+++ src/matrix/matrix_checker.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 15:00:14 by maolivei          #+#    #+#             */
-/*   Updated: 2022/10/21 12:01:52 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/26 09:44:03 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,6 @@ static double	_2x2_determinant(t_matrix *m)
 		(m->matrix[0][0] * m->matrix[1][1]) - \
 		(m->matrix[0][1] * m->matrix[1][0])
 	);
-}
-
-t_bool	is_equal_matrix(t_matrix *a, t_matrix *b)
-{
-	size_t	row;
-	size_t	col;
-
-	if (a->size != b->size)
-		return (FALSE);
-	row = 0;
-	while (row < a->size)
-	{
-		col = 0;
-		while (col < a->size)
-		{
-			if (!is_equal_double(a->matrix[row][col], b->matrix[row][col]))
-				return (FALSE);
-			++col;
-		}
-		++row;
-	}
-	return (TRUE);
 }
 
 double	determinant(t_matrix *m)
@@ -78,4 +56,26 @@ double	cofactor(t_matrix *m, size_t row, size_t col)
 	if ((row + col) % 2 != 0)
 		return (-_cofactor);
 	return (_cofactor);
+}
+
+t_bool	is_equal_matrix(t_matrix *a, t_matrix *b)
+{
+	size_t	row;
+	size_t	col;
+
+	if (a->size != b->size)
+		return (FALSE);
+	row = 0;
+	while (row < a->size)
+	{
+		col = 0;
+		while (col < a->size)
+		{
+			if (!is_equal_double(a->matrix[row][col], b->matrix[row][col]))
+				return (FALSE);
+			++col;
+		}
+		++row;
+	}
+	return (TRUE);
 }
