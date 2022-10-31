5de9ea06ff347f5925f709a4fc672e4459da4a55
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-10-31 10:42:25 -0300

    remove extra tmp variable in prepare_computation

--- src/world/world_computations.c
+++ src/world/world_computations.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:25:08 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/26 19:15:25 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/10/31 10:30:24 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 t_comp	*prepare_computation(t_intersect *i, t_ray *ray)
 {
 	t_comp		*comps;
-	t_vector	*aux1;
-	t_vector	*aux2;
+	t_vector	*aux;
 
 	comps = (t_comp *)malloc(sizeof(t_comp));
 	if (!comps)
@@ -29,14 +28,14 @@ t_comp	*prepare_computation(t_intersect *i, t_ray *ray)
 	if (dot_product(comps->normal, comps->camera) < 0)
 	{
 		comps->inside = TRUE;
-		aux1 = neg_tuple(comps->normal);
-		free(comps->normal);
-		comps->normal = aux1;
+		aux = comps->normal;
+		comps->normal = neg_tuple(comps->normal);
+		free(aux);
 	}
 	else
 		comps->inside = FALSE;
-	aux2 = scalar_multiply_tuple(comps->normal, EPSILON);
-	comps->over_point = sum_tuple(comps->point, aux2);
-	free(aux2);
+	aux = scalar_multiply_tuple(comps->normal, EPSILON);
+	comps->over_point = sum_tuple(comps->point, aux);
+	free(aux);
 	return (comps);
 }
