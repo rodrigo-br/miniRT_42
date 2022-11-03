75096900ee21f21235b65d8b841b254babf77965
rodrigo-br <rodrigoab123@gmail.com>
2022-11-03 18:47:58 -0300

    experimenting perturb_normal function on prepare computation

--- src/world/world_computations.c
+++ src/world/world_computations.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   world_computations.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:25:08 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/10/31 11:35:24 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/11/03 18:46:59 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,19 @@ t_vector	*normal_at(t_object *shape, t_point *point)
 
 	object_point = multiply_matrix_tuple(shape->inverse_transformation, point);
 	object_normal = shape->get_normal(shape, object_point);
+	if (object_normal->x > 0.5)
+		object_normal->x = 0.9;
+	else
+		object_normal->x = 0.1;
+	if (object_normal->y > 0.5)
+		object_normal->y = 0.9;
+	else
+		object_normal->y = 0.1;
+	if (object_normal->z > 0.5)
+		object_normal->z = 0.9;
+	else
+		object_normal->z = 0.1;
+	
 	transposed_inverse = transpose_matrix(shape->inverse_transformation);
 	world_normal = multiply_matrix_tuple(transposed_inverse, object_normal);
 	world_normal->w = VECTOR_W;
@@ -32,6 +45,29 @@ t_vector	*normal_at(t_object *shape, t_point *point)
 	world_normal = normalize(world_normal);
 	free(aux);
 	return (world_normal);
+}
+
+void	perturb_normal(t_vector *normal)
+{
+	t_vector	*new_up_vector;
+	t_tuple		*v;
+	t_tuple		*u;
+	t_tuple		*aux;
+
+	new_up_vector = &(t_tuple){0.0, 0.0, -1.0, 0};
+	if (normal->z > 0.99 || normal->z < -0.99)
+		new_up_vector = &(t_tuple){1.0, 0.0, 0.0, 0};
+	v = cross_product(new_up_vector, normal);
+	aux = normalize(v);
+	free(v);
+	v = aux;
+	u = cross_product(normal, v);
+	aux = normalize(u);
+	free(u);
+	u = aux;
+	aux = sum_tuple(sum_tuple(sum_tuple(normal, scalar_multiply_tuple(u, 0)), scalar_multiply_tuple(v, 0)), (scalar_multiply_tuple(normal, 0)));
+	free(normal);
+	normal = normalize(aux);
 }
 
 t_comp	*prepare_computation(t_intersect *i, t_ray *ray)
@@ -47,6 +83,7 @@ t_comp	*prepare_computation(t_intersect *i, t_ray *ray)
 	comps->point = get_position(ray, comps->time);
 	comps->camera = neg_tuple(ray->direction);
 	comps->normal = normal_at(comps->object, comps->point);
+	perturb_normal(comps->normal);
 	if (dot_product(comps->normal, comps->camera) < 0)
 	{
 		aux = comps->normal;
