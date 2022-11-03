f187951a85156330a3e4da3998a544287a5b9eba
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-11-03 19:58:11 -0300

    fix some tests and remove obsolete ones

--- tests/test_view_transformation.c
+++ tests/test_view_transformation.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 18:18:19 by maolivei          #+#    #+#             */
-/*   Updated: 2022/10/25 18:34:22 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/11/03 19:55:00 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,24 +68,29 @@ And to ← point(0, 0, 0)
 And up ← vector(0, 1, 0)
 When t ← view_transform(from, to, up)
 Then t = translation(0, 0, -8) */
-void	test_transformation_moves_the_world(void)
-{
-	t_matrix	*ts, *vt;
-	t_point		*from, *to;
-	t_vector	*up;
 
-	from = create_point(0, 0, 8);
-	to = create_point(0, 0, 0);
-	up = create_vector(0, 1, 0);
-	ts = translate_matrix(0, 0, -8);
-	vt = view_transform(from, to, up);
-	TEST_ASSERT_TRUE(is_equal_matrix(ts, vt));
-	free(from);
-	free(to);
-	free(up);
-	free(ts);
-	free(vt);
-}
+/* This test is now obsolete, because view transformation considers
+the already normalized vector coming from the '.rt' file. The calculus
+used to setup 'forward' by subtracting 'to' from 'from' is no longer
+performed. */
+// void	test_transformation_moves_the_world(void)
+// {
+// 	t_matrix	*ts, *vt;
+// 	t_point		*from, *to;
+// 	t_vector	*up;
+
+// 	from = create_point(0, 0, 8);
+// 	to = create_point(0, 0, 0);
+// 	up = create_vector(0, 1, 0);
+// 	ts = translate_matrix(0, 0, -8);
+// 	vt = view_transform(from, to, up);
+// 	TEST_ASSERT_TRUE(is_equal_matrix(ts, vt));
+// 	free(from);
+// 	free(to);
+// 	free(up);
+// 	free(ts);
+// 	free(vt);
+// }
 
 /* Scenario: An arbitrary view transformation
 Given from ← point(1, 3, 2)
@@ -97,35 +102,40 @@ Then t is the following 4x4 matrix:
 | 0.76772 | 0.60609 | 0.12122 | -2.82843 |
 | -0.35857 | 0.59761 | -0.71714 | 0.00000 |
 | 0.00000 | 0.00000 | 0.00000 | 1.00000 | */
-void	test_arbitrary_view_transformation(void)
-{
-	double		aux[MAT_MAX][MAT_MAX] = {
-		{ -0.50709, 0.50709, 0.67612, -2.36643 },
-		{ 0.76772, 0.60609, 0.12122, -2.82843 },
-		{ -0.35857, 0.59761, -0.71714, 0.00000 },
-		{ 0.00000, 0.00000, 0.00000, 1.00000 }
-	};
-	t_matrix	*ar, *vt;
-	t_point		*from, *to;
-	t_vector	*up;
 
-	from = create_point(1, 3, 2);
-	to = create_point(4, -2, 8);
-	up = create_vector(1, 1, 0);
-	ar = create_matrix(4, aux);
-	vt = view_transform(from, to, up);
-	TEST_ASSERT_TRUE(is_equal_matrix(ar, vt));
-	free(from);
-	free(to);
-	free(up);
-	free(ar);
-	free(vt);
-}
+/* This test is now obsolete, because view transformation considers
+the already normalized vector coming from the '.rt' file. The calculus
+used to setup 'forward' by subtracting 'to' from 'from' is no longer
+performed. */
+// void	test_arbitrary_view_transformation(void)
+// {
+// 	double		aux[MAT_MAX][MAT_MAX] = {
+// 		{ -0.50709, 0.50709, 0.67612, -2.36643 },
+// 		{ 0.76772, 0.60609, 0.12122, -2.82843 },
+// 		{ -0.35857, 0.59761, -0.71714, 0.00000 },
+// 		{ 0.00000, 0.00000, 0.00000, 1.00000 }
+// 	};
+// 	t_matrix	*ar, *vt;
+// 	t_point		*from, *to;
+// 	t_vector	*up;
+
+// 	from = create_point(1, 3, 2);
+// 	to = create_point(4, -2, 8);
+// 	up = create_vector(1, 1, 0);
+// 	ar = create_matrix(4, aux);
+// 	vt = view_transform(from, to, up);
+// 	TEST_ASSERT_TRUE(is_equal_matrix(ar, vt));
+// 	free(from);
+// 	free(to);
+// 	free(up);
+// 	free(ar);
+// 	free(vt);
+// }
 
 void	test_view_transformation(void)
 {
 	RUN_TEST(test_default_orientation);
 	RUN_TEST(test_positive_z_direction);
-	RUN_TEST(test_transformation_moves_the_world);
-	RUN_TEST(test_arbitrary_view_transformation);
+	// RUN_TEST(test_transformation_moves_the_world); /* obsolete, read above */
+	// RUN_TEST(test_arbitrary_view_transformation); /* obsolete, read above */
 }
